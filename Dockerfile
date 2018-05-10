cd16f3eb221a8014c18561d1a38b5f1edc50c628
Tony Apuzzo <tonyapuzzo@yahoo.com>
2018-05-10 15:35:24 -0600

    Parameterize both Alpine and Mosquitto versions.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM alpine:3.6
-MAINTAINER Tony Apuzzo <tonyapuzzo@yahoo.com>
+ARG ALPINE_VER=3.7
+FROM alpine:${ALPINE_VER}
 
+MAINTAINER Tony Apuzzo <tonyapuzzo@yahoo.com>
 LABEL Description="Raspberry Pi Eclipse Mosquitto MQTT Broker"
 
-ARG VERSION=1.4.15-r0
-
-RUN apk --no-cache add mosquitto="${VERSION}" && \
+ARG MOSQUITTO_VER=1.4.15-r0
+RUN apk --no-cache add mosquitto="${MOSQUITTO_VER}" && \
     mkdir -p /mosquitto/config /mosquitto/data /mosquitto/log && \
     cp /etc/mosquitto/mosquitto.conf /mosquitto/config && \
     chown -R mosquitto:mosquitto /mosquitto
--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 dir=$(dirname $0)
-cd "$dir"
-VERSION=1.4.15-r0
+cd "${dir}"
+ALPINE_VER=3.7
+MOSQUITTO_VER=1.4.15-r0
 BASE_TAG=tonyapuzzo/rpi-mosquitto
-docker build --build-arg VERSION="${VERSION}" --tag="${BASE_TAG}:${VERSION}" .
-docker tag "${BASE_TAG}:${VERSION}" "${BASE_TAG}:latest"
-docker push "${BASE_TAG}:${VERSION}"
+docker build \
+	--build-arg ALPINE_VER="${ALPINE_VER}" \
+	--build-arg MOSQUITTO_VER="${MOSQUITTO_VER}" \
+	--tag="${BASE_TAG}:${MOSQUITTO_VER}" .
+docker tag "${BASE_TAG}:${MOSQUITTO_VER}" "${BASE_TAG}:latest"
+docker push "${BASE_TAG}:${MOSQUITTO_VER}"
 docker push "${BASE_TAG}:latest"
